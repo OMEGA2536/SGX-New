f1d46420a785eeea5aada7d22dbd07648f47c891
JuanBallena <jpbu911@hotmail.com>
2022-11-22 00:20:03 -0500

    Paginator

--- next-ucv/src/app/system-users/company-search-cv/company-search-cv.component.ts
+++ next-ucv/src/app/system-users/company-search-cv/company-search-cv.component.ts
@@ -10,19 +10,50 @@ export class CompanySearchCvComponent implements OnInit {
   constructor() { }
 
   items: any[] = [];
+  visibleItems: any[] = [];
+  itemsPerPage: number = 10;
+  currentPage: number = 1;
+
+  startPosition: number = 0;
+  endPosition: number = 0;
 
   ngOnInit(): void {
 
-    for (let index = 0; index < 10; index++) {
+    for (let index = 0; index < 45; index++) {
 
       const item: any = {
         candidate: 'Oscar Eduardo Mendoza Ramos',
         course: 'Administración',
-        salary: 6000 + index
+        salary: 6000 + index + 1
       }
 
       this.items.push(item);
     }
+
+    this.render();
   }
 
+  render(): void {
+    this.visibleItems = [];
+
+    this.startPosition = this.itemsPerPage * (this.currentPage - 1);
+    this.endPosition = this.itemsPerPage * this.currentPage;
+
+    if (this.endPosition >= this.items.length) {
+      this.endPosition = this.items.length;
+    }
+
+    for (let index = this.startPosition; index < this.endPosition; index++) {
+      
+      this.visibleItems.push(this.items[index]);
+    }
+  }
+
+  onChangePage(page: number): void {
+
+    if (page === 0) return;
+
+    this.currentPage = page;
+    this.render();
+  }
 }
